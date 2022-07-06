8d3bd81c6856eea24b468d7f85052f668d76b133
Joss Sparkes <joss.sparkes@gmail.com>
2022-07-06 11:38:51 +0100

    Add POC performance testing framework  (#66)
    * Added POC framework currently with no Dicoms
    
    Signed-off-by: Joss Sparkes <joss.sparkes@gmail.com>
    
    * Remove LICENSE
    
    Signed-off-by: Joss Sparkes <joss.sparkes@gmail.com>
    
    * dotnet app for supporting performance tests
    
    Signed-off-by: Joe Batt <joe.batt@answerdigital.com>
    
    * Added readme
    
    Signed-off-by: Joe Batt <joe.batt@answerdigital.com>
    
    * Got the dotnet app working with the K6 scripts
    
    Signed-off-by: Joss Sparkes <joss.sparkes@gmail.com>
    
    Co-authored-by: Joe Batt <joe.batt@answerdigital.com>

---
+++ performance-testing/dotnet-performance-app/Support/DicomScu.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using FellowOakDicom;
+using FellowOakDicom.Network;
+using FellowOakDicom.Network.Client;
+
+namespace dotnet_performance_app.Support
+{
+    public class DicomScu
+    {
+        private static readonly object SyncRoot = new object();
+        internal int TotalTime { get; private set; } = 0;
+
+        public async Task<DicomStatus> CStore(string host, int port, string callingAeTitle, string calledAeTitle, IList<DicomFile> dicomFiles, TimeSpan timeout)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var dicomClient = CreateClient(host, port, callingAeTitle, calledAeTitle);
+            var countdownEvent = new CountdownEvent(dicomFiles.Count);
+            var failureStatus = new List<DicomStatus>();
+            foreach (var file in dicomFiles)
+            {
+                var cStoreRequest = new DicomCStoreRequest(file);
+                cStoreRequest.OnResponseReceived += (DicomCStoreRequest request, DicomCStoreResponse response) =>
+                {
+                    if (response.Status != DicomStatus.Success) failureStatus.Add(response.Status);
+                    countdownEvent.Signal();
+                };
+                await dicomClient.AddRequestAsync(cStoreRequest);
+            }
+
+            try
+            {
+                await dicomClient.SendAsync();
+                countdownEvent.Wait(timeout);
+                stopwatch.Stop();
+                lock (SyncRoot)
+                {
+                    TotalTime += (int)stopwatch.Elapsed.TotalMilliseconds;
+                }
+            }
+            catch (DicomAssociationRejectedException ex)
+            {
+                return DicomStatus.Cancel;
+            }
+
+            if (failureStatus.Count == 0) return DicomStatus.Success;
+
+            return failureStatus.First();
+        }
+
+        private IDicomClient CreateClient(string host, int port, string callingAeTitle, string calledAeTitle)
+        {
+            return DicomClientFactory.Create(host, port, false, callingAeTitle, calledAeTitle);
+        }
+    }
+}
